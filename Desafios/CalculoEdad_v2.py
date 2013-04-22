3a58a79dd6dc7798f16db766de9307cfc3cec5fc
David Guerrero <deivguerrero@gmail.com>
2013-04-22 11:35:19 -0500

    COMMIT 2: Se agregaron los comentarios

--- Desafios/CalculoEdad_v2.py
+++ Desafios/CalculoEdad_v2.py
@@ -1,15 +1,32 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
+#Definimos una funcion inicial, el nombre es lo de menos
 def init():
+    """El except sirven para capturar errores que surjan
+       al momento de ejecutar las operaciones que estan dentro
+       del try
+    """
     try:
+        #Solicitamos el año actual y lo convertimos a entero
         anioActual = int(raw_input("Año Actual:"))
+        
         for i in range(3):
+            #llamamos la funcion elTipo y le pasamos como parametro anioActual
             elTipo(anioActual)
+            
+    #El except capturara los errores de tipo ValueError esto es si en dado caso
+    #al convertir (linea 12) la entrada no sean numeros (que hayan introducido
+    #alguna letra u otro caracter)"""
     except ValueError:
+        #Le enviamos un mensaje al usuario
         print "Debes introducir un año válido (dígitos)\nVuelve a intentar\n\n"
+        #volvemos a llamar la función init
         init()
 
+"""La función elTipo lleva el mismo patrón de procedimiento que la 
+   función init (refirendo al try,except)
+"""
 def elTipo(anioActual):
     try:
         nom = raw_input("Nombre:")
